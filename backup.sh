0ac9bc873d3996b1843fb034c99f1f8c74883711
lik-alive <kirshdv@gmail.com>
2021-09-10 21:05:08 +0400

    r

--- backup.sh
+++ backup.sh
@@ -1,14 +1,18 @@
-echo 'comaster1 backup started'
-ROOT=$1/comaster1
+title=comaster1
+
+echo $title backup started
+ROOT=$1/$title
 mkdir $ROOT
 
 # Backup web files
 WEBPATH=$ROOT/web
 mkdir $WEBPATH
-rsync -a --info=progress2 . $WEBPATH --exclude .git
+#rsync -a --info=progress2 . $WEBPATH --exclude .git
+tar cf - --exclude ".git" . | pv -s $(du -sb . | awk '{print $1}') | gzip > $WEBPATH/$title.tar.gz
 
 # Backup db
 DBPATH=$ROOT/db
 mkdir $DBPATH
 mysqldump -u$2 -p$3 --databases co1db > $DBPATH/dump.sql 2> /dev/null
-echo 'comaster1 backup finished'
+
+echo $title backup finished
